c882edd6dfd7e48d60b37b2c4f79951456e3b863
eranpeer <jet.actors@gmail.com>
2013-09-18 13:42:54 +0300

    split data member tests into new file

---
+++ mock4cpp/data_members_tests.cpp
@@ -0,0 +1,137 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include "Mock.h"
+#include <iostream>
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+
+namespace mock4cpp_tests
+{
+
+	struct ReferenceInterface {
+		virtual int& func1() = 0;
+		virtual ReferenceInterface& func2() = 0;
+		virtual void proc1(int&) = 0;
+		virtual void proc2(ReferenceInterface &) = 0;
+	};
+
+	static bool operator==(const ReferenceInterface& a, const ReferenceInterface& b)
+	{
+		return (&a == &b);
+	}
+
+	struct PointerInterface {
+		virtual int* func1() = 0;
+		virtual PointerInterface* func2() = 0;
+
+		virtual void proc1(int*) = 0;
+		virtual void proc2(PointerInterface *) = 0;
+	};
+
+	TEST_CLASS(DataMembersTest)
+	{
+	public:
+
+		struct AbstractClass
+		{
+			AbstractClass() : c(1), s(1), i(1), f(1), d(1), l(1){}
+
+			char c;
+			short s;
+			int i;
+			float f;
+			double d;
+			long l;
+
+			std::string str;
+			int iArr[3];
+
+			virtual void proc() = 0;
+		};
+
+		TEST_METHOD(DataMembersAreInitiatedToZero){
+			Mock<AbstractClass> mock;
+			AbstractClass &i = mock.get();
+			Assert::AreEqual((char) 0, i.c);
+			Assert::AreEqual((short) 0, i.s);
+			Assert::AreEqual((int) 0, i.i);
+			Assert::AreEqual((float) 0, i.f);
+			Assert::AreEqual((double) 0, i.d);
+			Assert::AreEqual((long) 0, i.l);
+		}
+
+		TEST_METHOD(SetGetPrimitiveDataMembers){
+			Mock<AbstractClass> mock;
+			AbstractClass &i = mock.get();
+			i.c = 1;
+			i.s = 1;
+			i.i = 1;
+			i.f = 1;
+			i.d = 1;
+			i.l = 1;
+			Assert::AreEqual((char) 1, i.c);
+			Assert::AreEqual((short) 1, i.s);
+			Assert::AreEqual((int) 1, i.i);
+			Assert::AreEqual((float) 1.0, i.f);
+			Assert::AreEqual((double) 1.0, i.d);
+			Assert::AreEqual((long) 1, i.l);
+		}
+
+		TEST_METHOD(StubPrimitiveDataMembers){
+			Mock<AbstractClass> mock;
+			AbstractClass &i = mock.get();
+			mock.Stub(&AbstractClass::c, '1');
+			mock.Stub(&AbstractClass::s, (short) 2);
+			mock.Stub(&AbstractClass::i, 3);
+			mock.Stub(&AbstractClass::f, (float) 4);
+			mock.Stub(&AbstractClass::d, (double) 5);
+			mock.Stub(&AbstractClass::l, 6);
+			Assert::AreEqual('1', i.c);
+			Assert::AreEqual((short) 2, i.s);
+			Assert::AreEqual((int) 3, i.i);
+			Assert::AreEqual((float) 4.0, i.f);
+			Assert::AreEqual((double) 5.0, i.d);
+			Assert::AreEqual((long) 6, i.l);
+		}
+
+		TEST_METHOD(StubObjectDataMembers){
+			Mock<AbstractClass> mock;
+			AbstractClass &i = mock.get();
+
+			mock.Stub(&AbstractClass::str);
+			Assert::AreEqual(std::string(), i.str);
+
+			mock.Stub(&AbstractClass::str, "text");
+			Assert::AreEqual(std::string("text"), i.str);
+
+			mock.Stub(&AbstractClass::str, 'a', 'b');
+			Assert::AreEqual(std::string("ab"), i.str);
+		}
+
+		TEST_METHOD(StubArrayDataMembers){
+			Mock<AbstractClass> mock;
+			AbstractClass &i = mock.get();
+
+			mock.Stub(&AbstractClass::iArr);
+			Assert::AreEqual(0, i.iArr[0]);
+			Assert::AreEqual(0, i.iArr[1]);
+			Assert::AreEqual(0, i.iArr[2]);
+
+			mock.Stub(&AbstractClass::iArr, 1);
+			Assert::AreEqual(1, i.iArr[0]);
+			Assert::AreEqual(0, i.iArr[1]);
+			Assert::AreEqual(0, i.iArr[2]);
+
+			mock.Stub(&AbstractClass::iArr, 1, 2);
+			Assert::AreEqual(1, i.iArr[0]);
+			Assert::AreEqual(2, i.iArr[1]);
+			Assert::AreEqual(0, i.iArr[2]);
+
+			mock.Stub(&AbstractClass::iArr, 1, 2, 3);
+			Assert::AreEqual(1, i.iArr[0]);
+			Assert::AreEqual(2, i.iArr[1]);
+			Assert::AreEqual(3, i.iArr[2]);
+		}
+	};
+}
--- mock4cpp/stubbing_tests.cpp
+++ mock4cpp/stubbing_tests.cpp
@@ -40,22 +40,6 @@ namespace mock4cpp_tests
 			virtual void proc(int) = 0;
 		};
 
-		TEST_METHOD(StubAllCallsToDefaultBeavior)
-		{
-			Mock<SomeInterface> mock;
-			mock.Stub(&SomeInterface::func);
-			mock.Stub(&SomeInterface::proc);
-
-			SomeInterface &i = mock.get();
-
-			//Default behavior of a function is to return 0/false/null/default ctor
-			Assert::AreEqual(0, i.func(1));
-			Assert::AreEqual(0, i.func(2));
-
-			// Default behavior of a procedure is to do nothing
-			i.proc(1);
-			i.proc(2);
-		}
 
 		TEST_METHOD(StubAllCallsToAlternateBeavior)
 		{
@@ -65,8 +49,8 @@ namespace mock4cpp_tests
 
 			SomeInterface &i = mock.get();
 
-			Assert::AreEqual(1, i.func(1), L"should return 1");
-			Assert::AreEqual(1, i.func(2), L"should return 1");
+			Assert::AreEqual(1, i.func(1));
+			Assert::AreEqual(1, i.func(2));
 
 			Assert::ExpectException<std::string>([&i]{ i.proc(1); });
 			Assert::ExpectException<std::string>([&i]{ i.proc(2); });
@@ -126,8 +110,8 @@ namespace mock4cpp_tests
 
 		TEST_METHOD(StubOnlySpecifiedCallsToAlternateBehavior_WithExplicitDefaultBehavior)
 		{
-			std::function<int(int) > funcStub = [](int a){return 1; };
-			std::function<void(int) > procStub = [](int a){ throw a; };
+			std::function<int(int) > funcStub = [](...){return 1; };
+			std::function<void(int) > procStub = [](...){ throw 1; };
 
 			Mock<SomeInterface> mock;
 			mock.Stub(&SomeInterface::func).Do(funcStub);
@@ -218,120 +202,6 @@ namespace mock4cpp_tests
 			i.proc(1);
 		}
 
-		struct AbstractClass
-		{
-			AbstractClass() : c(1), s(1), i(1), f(1), d(1), l(1){}
- 			
-			char c;
-			short s;
-			int i;
-			float f;
-			double d;
-			long l;
-
-			std::string str;
-			int iArr[3];
-
-			virtual void proc() = 0;
-		};
-
-		TEST_METHOD(DataMembersAreInitiatedToZero){
-			Mock<AbstractClass> mock;
-			AbstractClass &i = mock.get();
-			Assert::AreEqual((char) 0, i.c);
-			Assert::AreEqual((short)0, i.s);
-			Assert::AreEqual((int) 0, i.i);
-			Assert::AreEqual((float) 0, i.f);
-			Assert::AreEqual((double) 0, i.d);
-			Assert::AreEqual((long) 0, i.l);
-		}
-
-		TEST_METHOD(SetGetPrimitiveDataMembers){
-			Mock<AbstractClass> mock;
-			AbstractClass &i = mock.get();
-			i.c = 1;
-			i.s = 1;
-			i.i = 1;
-			i.f = 1;
-			i.d = 1;
-			i.l = 1;
-			Assert::AreEqual((char) 1, i.c);
-			Assert::AreEqual((short) 1, i.s);
-			Assert::AreEqual((int) 1, i.i);
-			Assert::AreEqual((float) 1.0, i.f);
-			Assert::AreEqual((double) 1.0, i.d);
-			Assert::AreEqual((long) 1, i.l);
-		}
-
-		TEST_METHOD(StubPrimitiveDataMembers){
-			Mock<AbstractClass> mock;
-			AbstractClass &i = mock.get();
-			mock.Stub(&AbstractClass::c, '1');
-			mock.Stub(&AbstractClass::s, (short)2);
-			mock.Stub(&AbstractClass::i, 3);
-			mock.Stub(&AbstractClass::f, (float)4);
-			mock.Stub(&AbstractClass::d, (double)5);
-			mock.Stub(&AbstractClass::l, 6);
-			Assert::AreEqual('1', i.c);
-			Assert::AreEqual((short) 2, i.s);
-			Assert::AreEqual((int) 3, i.i);
-			Assert::AreEqual((float) 4.0, i.f);
-			Assert::AreEqual((double) 5.0, i.d);
-			Assert::AreEqual((long) 6, i.l);
-		}
-
-		TEST_METHOD(StubObjectDataMembers){
-			Mock<AbstractClass> mock;
-			AbstractClass &i = mock.get();
-
-			mock.Stub(&AbstractClass::str);
-			Assert::AreEqual(std::string(), i.str);
-			
-			mock.Stub(&AbstractClass::str,"text");
-			Assert::AreEqual(std::string("text"), i.str);
-			
-			mock.Stub(&AbstractClass::str, 'a','b');
-			Assert::AreEqual(std::string("ab"), i.str);
-		}
-
-		TEST_METHOD(StubArrayDataMembers){
-			Mock<AbstractClass> mock;
-			AbstractClass &i = mock.get();
-
-			mock.Stub(&AbstractClass::iArr);
-			Assert::AreEqual(0, i.iArr[0]);
-			Assert::AreEqual(0, i.iArr[1]);
-			Assert::AreEqual(0, i.iArr[2]);
-
-			mock.Stub(&AbstractClass::iArr, 1);
-			Assert::AreEqual(1, i.iArr[0]);
-			Assert::AreEqual(0, i.iArr[1]);
-			Assert::AreEqual(0, i.iArr[2]);
-
-			mock.Stub(&AbstractClass::iArr, 1, 2);
-			Assert::AreEqual(1, i.iArr[0]);
-			Assert::AreEqual(2, i.iArr[1]);
-			Assert::AreEqual(0, i.iArr[2]);
-
-			mock.Stub(&AbstractClass::iArr, 1, 2, 3);
-			Assert::AreEqual(1, i.iArr[0]);
-			Assert::AreEqual(2, i.iArr[1]);
-			Assert::AreEqual(3, i.iArr[2]);
-		}
-
-
-		// #include "../mockutils/MethodProxy.h"
-		// #include "../mockutils/is_equality_comparable.hpp"
-		// 
-		// template <typename T>
-		// typename std::enable_if <std::is_class<T>::value && !is_equality_comparable<T>::value, bool>::type
-		// 	operator==(const T& a, const T& b)
-		// {
-		// 	return (&a == &b);
-		// }
-
-
-
 		TEST_METHOD(StubProcWithReferenceParams){
 			Mock<ReferenceInterface> mock;
 			mock.Stub(&ReferenceInterface::proc1);
@@ -340,13 +210,13 @@ namespace mock4cpp_tests
 			mock.Stub(&ReferenceInterface::func2);
 
 			ReferenceInterface & i = mock.get();
-			ReferenceInterface* pReferenceInterface { nullptr };
+			ReferenceInterface* pReferenceInterface{ nullptr };
 			ReferenceInterface& ref = *pReferenceInterface;
-			int a{0};
+			int a{ 0 };
 			i.proc1(a);
 			i.proc2(ref);
 			int& rInt = i.func1();
-			Assert::AreEqual(0,rInt,L"fundamental types are initiated to 0");
+			Assert::AreEqual(0, rInt, L"fundamental types are initiated to 0");
 			Assert::ExpectException<std::string>([&i]{ i.func2(); }, L"should fail to create default value");
 		}
 
@@ -362,63 +232,5 @@ namespace mock4cpp_tests
 			Assert::IsNull(i.func1());
 			Assert::IsNull(i.func2());
 		}
-
-
-		template<typename... arglist>
-		struct ArgsHolder {
-
-			ArgsHolder(const arglist&... args) : tuple(args...){
-			}
- 			
-			bool match(const arglist& ... args){
-				return tuple == std::tuple<arglist...>(args...);
-			}
-
-		private:
-			std::tuple<arglist...> tuple;
-		};
-
-// 		void f(const int&& a){
-// 		}
-// 
-// 		void f(const int & a){
-// 		}
-
-// 		TEST_METHOD(TestTuple){
-// 			int a = 1;
-// 			int& ar = a;
-// 			int b = 1;
-// 			int& br = b;
-// 
-// 			f(ar);
-//  			ArgsHolder<int, int> ah(ar, a);
-//  			Assert::IsTrue(ah.match(1, 1));
-// 		}
-
-// 		TEST_METHOD(StubWithoutWhenClouse_ShouldStubAllCallsToDefaultBeaviour)
-// 		{
-// 			Mock<PrimitiveFunctions> mock;
-// 			mock.Stub(&PrimitiveFunctions::fBool);
-// 			mock.Stub(&PrimitiveFunctions::fShort);
-// 			mock.Stub(&PrimitiveFunctions::fUnsignedShort);
-// 			mock.Stub(&PrimitiveFunctions::fInt);
-// 			mock.Stub(&PrimitiveFunctions::fUnsignedInt);
-// 			mock.Stub(&PrimitiveFunctions::fLong);
-// 			mock.Stub(&PrimitiveFunctions::fUnsignedLong);
-// 			mock.Stub(&PrimitiveFunctions::fFloat);
-// 			mock.Stub(&PrimitiveFunctions::fDouble);
-// 
-// 			PrimitiveFunctions &i = mock.get();
-// 			Assert::IsTrue(false == i.fBool());
-// 			Assert::IsTrue(0 == i.fShort());
-// 			Assert::IsTrue(0 == i.fUnsignedShort());
-// 			Assert::IsTrue(0 == i.fInt());
-// 			Assert::IsTrue(0 == i.fUnsignedInt());
-// 			Assert::IsTrue(0 == i.fLong());
-// 			Assert::IsTrue(0 == i.fUnsignedLong());
-// 			Assert::IsTrue(0 == i.fFloat());
-// 			Assert::IsTrue(0 == i.fDouble());
-// 		}
-		
 	};
 }
