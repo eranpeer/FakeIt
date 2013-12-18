56ebfec0ec5c12541aa0316dd911c1104a7be7b2
eranpeer <jet.actors@gmail.com>
2013-12-18 10:15:06 +0200

    make DefaultValue compile on gcc

--- mock4cpp/include/mockutils/DefaultValue.hpp
+++ mock4cpp/include/mockutils/DefaultValue.hpp
@@ -13,7 +13,7 @@ struct DefaultValue {
 	{
 		int a = 0;
 		a++;
-	}
+		}
 
 	// Type is default constructible => It can have a default value.
 	//template<typename C, class REF = std::add_reference<C>::type>
@@ -29,37 +29,37 @@ struct DefaultValue {
 	{
 		static C val{};
 		return val;
-	}
+		}
 
 	// Can't create default value of an a non default constructible type.
-	template<typename C, class REF = std::add_reference<C>::type>
-	static typename 
+	template<typename C>
+	static typename
 		std::enable_if<
 		!std::is_void<C>::value &&
 		!std::is_reference<C>::value &&
-		!std::is_abstract<C>::value && 	
+		!std::is_abstract<C>::value &&
 		!std::is_default_constructible<C>::value
-		, REF>::type 
-	value()
+		, C&>::type
+		value()
 	{
 		throw std::string(typeid(C).name()) + std::string(" does not have a default constructor. could not instantiate default value for type");
-	}
+		}
 
 	// Can't create default value of an abstract type
-	template<typename AC, class REF = std::add_reference<AC>::type>
+	template<typename AC>
 	static typename std::enable_if<
 		!std::is_reference<AC>::value &&
 		std::is_abstract<AC>::value
-		, REF>::type 
-	value()
+		, AC&>::type
+		value()
 	{
 		throw std::string(typeid(AC).name()) + std::string(" is abstract. could not instantiate default value for class");
-	}
+		}
 
-	template<typename REF, class T = std::remove_reference<REF>::type>
+	template<typename REF>
 	static typename std::enable_if<std::is_reference<REF>::value, REF>::type value()
 	{
-		return DefaultValue::value<T>();
+		return DefaultValue::value<std::remove_reference<REF>::type>();
 	}
 
 };
