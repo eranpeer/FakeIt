f24423e1a6b1864c253c9449a5216f44d621d966
Eran Peer <eran@localhost.localdomain>
2013-12-21 06:44:59 -0500

    add tests

--- mock4cpp/tests/mock4cpp_tests/stubbing_tests.cpp
+++ mock4cpp/tests/mock4cpp_tests/stubbing_tests.cpp
@@ -1,12 +1,16 @@
 #include "tpunit++.hpp"
 #include "mock4cpp.h"
+#include <string>
 
 struct BasicStubbing: tpunit::TestFixture {
 	BasicStubbing() :
-			tpunit::TestFixture( //
+			tpunit::TestFixture(
+					//
 					TEST(BasicStubbing::calling_an_unstubbed_method_should_raise_UnmockedMethodException), //
 					TEST(BasicStubbing::stubbing_method_to_default_behaviore), //
-					TEST(BasicStubbing::test_b)  //
+					TEST(BasicStubbing::stubbing_multiple_methods_to_default_behaviore),
+					TEST(BasicStubbing::stubbing_a_function_and_returning_a_specified_value),
+					TEST(BasicStubbing::stubbing_a_method_and_throwing_a_specified_exception) //
 							)  //
 	{
 	}
@@ -20,21 +24,73 @@ struct BasicStubbing: tpunit::TestFixture {
 	void calling_an_unstubbed_method_should_raise_UnmockedMethodException() {
 		Mock<SomeInterface> mock;
 		SomeInterface &i = mock.get();
-		ASSERT_THROW(i.func(1),UnmockedMethodException);
-		ASSERT_THROW(i.proc(1),UnmockedMethodException);
+		ASSERT_THROW(i.func(1), UnmockedMethodException);
+		ASSERT_THROW(i.proc(1), UnmockedMethodException);
 	}
 
-
 	void stubbing_method_to_default_behaviore() {
-			Mock<SomeInterface> mock;
+		Mock<SomeInterface> mock;
 
-			Stub(mock[&SomeInterface::func]);
-			Stub(mock[&SomeInterface::proc]);
+		Stub(mock[&SomeInterface::func]);
+		Stub(mock[&SomeInterface::proc]);
 
-			SomeInterface &i = mock.get();
+		SomeInterface &i = mock.get();
 
-			ASSERT_EQUAL(0,i.func(1));
-			ASSERT_NO_THROW(i.proc(1));
+		ASSERT_EQUAL(0, i.func(1));
+		ASSERT_NO_THROW(i.proc(1));
+	}
+
+	void stubbing_multiple_methods_to_default_behaviore() {
+		Mock<SomeInterface> mock;
+
+		Stub(mock[&SomeInterface::func], mock[&SomeInterface::proc]);
+
+		SomeInterface &i = mock.get();
+
+		ASSERT_EQUAL(0, i.func(1));
+		ASSERT_NO_THROW(i.proc(1));
+	}
+
+	void stubbing_a_method_to_default_behaviore() {
+		Mock<SomeInterface> mock;
+
+		Stub(mock[&SomeInterface::func], mock[&SomeInterface::proc]);
+
+		SomeInterface &i = mock.get();
+
+		ASSERT_EQUAL(0, i.func(1));
+		ASSERT_NO_THROW(i.proc(1));
+	}
+
+	void stubbing_a_function_and_returning_a_specified_value() {
+		Mock<SomeInterface> mock;
+
+		When(mock[&SomeInterface::func]).Return(1);
+
+		SomeInterface &i = mock.get();
+
+		ASSERT_EQUAL(1, i.func(1));
+	}
+
+	void stubbing_a_method_and_throwing_a_specified_exception() {
+		Mock<SomeInterface> mock;
+
+		When(mock[&SomeInterface::func]).Throw(std::string("exception"));
+		When(mock[&SomeInterface::proc]).Throw(std::string("exception"));
+
+		SomeInterface &i = mock.get();
+
+		try {
+			i.func(1);
+		} catch (std::string e) {
+			ASSERT_EQUAL(std::string("exception"), e);
+		}
+
+		try {
+			i.proc(1);
+		} catch (std::string e) {
+			ASSERT_EQUAL(std::string("exception"), e);
+		}
 	}
 
 	void test_a() {
