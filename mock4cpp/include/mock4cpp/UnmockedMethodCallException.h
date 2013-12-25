e7aed6d28d2dc41eee6adba0e06e5d46e612ef27
eranpeer <jet.actors@gmail.com>
2013-12-25 10:45:06 +0200

    add exception text message

--- mock4cpp/include/mock4cpp/UnmockedMethodCallException.h
+++ mock4cpp/include/mock4cpp/UnmockedMethodCallException.h
@@ -5,6 +5,7 @@
 namespace mock4cpp {
 
 	struct UnmockedMethodCallException : public std::exception {
+		UnmockedMethodCallException() : std::exception("Could not find recorded implementation to support method call") {}
 	};
 
 }
