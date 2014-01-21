6bc6cfb284835274f5bf1e30443d070743263e8b
Eran Peer <eran@localhost.localdomain>
2014-01-21 15:07:01 -0500

    improve gcc verification tests

--- mock4cpp/tests/mock4cpp_tests/verification_tests.cpp
+++ mock4cpp/tests/mock4cpp_tests/verification_tests.cpp
@@ -14,10 +14,10 @@ struct BasicVerification: tpunit::TestFixture {
 					TEST(BasicVerification::verify_method_was_never_called), //
 					TEST(BasicVerification::verify_method_was_called_exactly_x_times), //
 					TEST(BasicVerification::should_throw_IllegalArgumentException_on_negative_times_argument), //
-					TEST(BasicVerification::verify_with_filter),//
-					TEST(BasicVerification::verify_concatenated_sequence),
-					TEST(BasicVerification::verify_repeated_sequence)
-					)  //
+					TEST(BasicVerification::verify_with_filter), //
+					TEST(BasicVerification::verify_concatenated_sequence), //
+					TEST(BasicVerification::verify_repeated_sequence), //
+					TEST(BasicVerification::verify_multi_sequences_in_order))  //
 	{
 	}
 
@@ -172,7 +172,8 @@ struct BasicVerification: tpunit::TestFixture {
 		Verify(mock[&SomeInterface::func] + mock[&SomeInterface::func]).Twice();
 		Verify(mock[&SomeInterface::func].Using(1) + mock[&SomeInterface::func].Using(3)).Never();
 
-		ASSERT_THROW(Verify(mock[&SomeInterface::func].Using(1) + mock[&SomeInterface::func].Using(3)),mock4cpp::MethodCallVerificationException);
+		ASSERT_THROW(Verify(mock[&SomeInterface::func].Using(1) + mock[&SomeInterface::func].Using(3)),
+				mock4cpp::MethodCallVerificationException);
 	}
 
 	void verify_repeated_sequence() {
@@ -194,11 +195,86 @@ struct BasicVerification: tpunit::TestFixture {
 		Verify(mock[&SomeInterface::func] * 5).Times(1);
 		Verify(mock[&SomeInterface::func] * 6).Times(1);
 
-		Verify(mock[&SomeInterface::func].Using(1) + mock[&SomeInterface::func].Using(2) +mock[&SomeInterface::func].Using(3)).Twice();
-		Verify((mock[&SomeInterface::func].Using(1) + mock[&SomeInterface::func].Using(2) +mock[&SomeInterface::func].Using(3))*2).Once();
+		Verify(mock[&SomeInterface::func].Using(1) + mock[&SomeInterface::func].Using(2) + mock[&SomeInterface::func].Using(3)).Twice();
+		Verify((mock[&SomeInterface::func].Using(1) + mock[&SomeInterface::func].Using(2) + mock[&SomeInterface::func].Using(3)) * 2).Once();
 		Verify(mock[&SomeInterface::func].Using(1) * 2).Never();
 
-		ASSERT_THROW(Verify(mock[&SomeInterface::func].Using(1) * 2),mock4cpp::MethodCallVerificationException);
+		ASSERT_THROW(Verify(mock[&SomeInterface::func].Using(1) * 2), mock4cpp::MethodCallVerificationException);
+	}
+
+	void verify_multi_sequences_in_order() {
+		Mock<SomeInterface> mock;
+		Stub(mock[&SomeInterface::func], mock[&SomeInterface::proc]);
+		SomeInterface &i = mock.get();
+
+		i.func(1);
+		i.func(2);
+		i.func(3);
+		i.func(1);
+		i.func(2);
+		i.func(3);
+
+		Verify(  //
+				mock[&SomeInterface::func],  //
+				mock[&SomeInterface::func],  //
+				mock[&SomeInterface::func],  //
+				mock[&SomeInterface::func],  //
+				mock[&SomeInterface::func],  //
+				mock[&SomeInterface::func])  //
+		.Times(1);
+
+		Verify(  //
+				mock[&SomeInterface::func],  //
+				mock[&SomeInterface::func],  //
+				mock[&SomeInterface::func],  //
+				mock[&SomeInterface::func],  //
+				mock[&SomeInterface::func])  //
+		.Times(1);
+
+		Verify(  //
+				mock[&SomeInterface::func],  //
+				mock[&SomeInterface::func],  //
+				mock[&SomeInterface::func],  //
+				mock[&SomeInterface::func])  //
+		.Times(1);
+
+		Verify(  //
+				mock[&SomeInterface::func],  //
+				mock[&SomeInterface::func],  //
+				mock[&SomeInterface::func])  //
+		.Times(2);
+
+		Verify(  //
+				mock[&SomeInterface::func],  //
+				mock[&SomeInterface::func])  //
+		.Times(3);
+
+		Verify(  //
+				mock[&SomeInterface::func])  //
+		.Times(6);
+
+		Verify(  //
+				mock[&SomeInterface::func].Using(1) +  //
+						mock[&SomeInterface::func].Using(2) + //
+						mock[&SomeInterface::func].Using(3))  //
+		.Times(2);
+
+		Verify(  //
+				mock[&SomeInterface::func].Using(1) +  //
+						mock[&SomeInterface::func].Using(2))  //
+		.Times(2);
+
+		Verify(  //
+				mock[&SomeInterface::func].Using(2) +  //
+						mock[&SomeInterface::func].Using(3))  //
+		.Times(2);
+
+		Verify(  //
+				mock[&SomeInterface::func].Using(1))  //
+		.Times(2);
+
+		Verify(mock[&SomeInterface::func].Using(2) +  //
+				mock[&SomeInterface::func].Using(1)).Never();
 	}
 //
 } __BasicVerification;
