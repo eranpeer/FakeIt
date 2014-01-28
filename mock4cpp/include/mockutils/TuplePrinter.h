b50ed02f52f815e186ab5bf641c2db46e45652d6
eranpeer <jet.actors@gmail.com>
2014-01-28 15:18:26 +0200

    work on error msg formatting

--- mock4cpp/include/mockutils/TuplePrinter.h
+++ mock4cpp/include/mockutils/TuplePrinter.h
@@ -7,39 +7,42 @@
 #include <sstream>
 #include "mockutils/Formatter.h"
 
-// helper function to print a tuple of any size
-template<class Tuple, std::size_t N>
-struct TuplePrinter {
-	static void print(std::ostream &strm, const Tuple& t) {
-		TuplePrinter<Tuple, N - 1>::print(strm, t);
-		strm << ", " << mock4cpp::Formatter<decltype(std::get<N - 1>(t))>::format(std::get<N - 1>(t));
-	}
-};
+namespace mock4cpp {
 
-template<class Tuple>
-struct TuplePrinter<Tuple, 1> {
-	static void print(std::ostream &strm, const Tuple& t) {
-		strm << mock4cpp::Formatter<decltype(std::get<0>(t))>::format(std::get<0>(t));
-	}
-};
+	// helper function to print a tuple of any size
+	template<class Tuple, std::size_t N>
+	struct TuplePrinter {
+		static void print(std::ostream &strm, const Tuple& t) {
+			TuplePrinter<Tuple, N - 1>::print(strm, t);
+			strm << ", " << mock4cpp::Formatter<decltype(std::get<N - 1>(t))>::format(std::get<N - 1>(t));
+		}
+	};
 
-template<class Tuple>
-struct TuplePrinter<Tuple, 0> {
-	static void print(std::ostream &strm, const Tuple& t) {
-	}
-};
+	template<class Tuple>
+	struct TuplePrinter<Tuple, 1> {
+		static void print(std::ostream &strm, const Tuple& t) {
+			strm << mock4cpp::Formatter<decltype(std::get<0>(t))>::format(std::get<0>(t));
+		}
+	};
 
-template<class ... Args>
-void print(std::ostream &strm, const std::tuple<Args...>& t) {
-	strm << "(";
-	TuplePrinter<decltype(t), sizeof...(Args)>::print(strm,t);
-	strm << ")";
+	template<class Tuple>
+	struct TuplePrinter<Tuple, 0> {
+		static void print(std::ostream &strm, const Tuple& t) {
+		}
+	};
+
+	template<class ... Args>
+	void print(std::ostream &strm, const std::tuple<Args...>& t) {
+		strm << "(";
+		TuplePrinter<decltype(t), sizeof...(Args)>::print(strm, t);
+		strm << ")";
+	}
+
+	template<class ... Args>
+	std::ostream & operator<<(std::ostream &strm, const std::tuple<Args...>& t) {
+		print(strm, t);
+		return strm;
+	}
+
 }
-
-template<class ... Args>
-std::ostream & operator<<(std::ostream &strm, const std::tuple<Args...>& t) {
-	print(strm, t);
-	return strm;
-}
-
 #endif // TuplePrinter_h__
