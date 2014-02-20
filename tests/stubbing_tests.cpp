db5df9c07221b1fd7b37968253d0d092936568c5
eranpeer <eranpe@gmail.com>
2014-02-19 23:54:43 -0500

    add unit tests

--- tests/stubbing_tests.cpp
+++ tests/stubbing_tests.cpp
@@ -9,11 +9,13 @@ struct BasicStubbing: tpunit::TestFixture {
 			tpunit::TestFixture(
 					//
 					TEST(BasicStubbing::calling_an_unstubbed_method_should_raise_UnmockedMethodCallException), //
-					TEST(BasicStubbing::stub_method_to_default_behaviore), //
+					TEST(BasicStubbing::stub_method_to_default_behaviore_will_always_return_do_the_default_behaviore), //
 					TEST(BasicStubbing::stub_multiple_methods_to_default_behaviore),
-					TEST(BasicStubbing::stub_a_function_to_return_a_specified_value),
-					TEST(BasicStubbing::stub_a_method_to_throw_a_specified_exception), //
-					TEST(BasicStubbing::stub_a_method_with_lambda_delegate), //
+					TEST(BasicStubbing::stub_a_function_to_return_a_specified_value_once),
+					TEST(BasicStubbing::stub_a_function_to_return_a_specified_value_always),
+					TEST(BasicStubbing::stub_a_method_to_throw_a_specified_exception_once), //
+					TEST(BasicStubbing::stub_a_method_with_lambda_delegate_once), //
+					TEST(BasicStubbing::stub_a_method_with_lambda_delegate_always), //
 					TEST(BasicStubbing::stub_a_method_with_static_method_delegate), //
 					TEST(BasicStubbing::stub_by_assignment_with_lambda_delegate), //
 					TEST(BasicStubbing::stub_by_assignment_with_static_method_delegate), //
@@ -41,13 +43,16 @@ struct BasicStubbing: tpunit::TestFixture {
 		ASSERT_THROW(i.proc(1), UnmockedMethodCallException);
 	}
 
-	void stub_method_to_default_behaviore() {
+	void stub_method_to_default_behaviore_will_always_return_do_the_default_behaviore() {
 		Mock<SomeInterface> mock;
 
 		Stub(mock[&SomeInterface::func]);
 		Stub(mock[&SomeInterface::proc]);
 
 		SomeInterface &i = mock.get();
+
+		ASSERT_EQUAL(0, i.func(1));
+		ASSERT_NO_THROW(i.proc(1));
 
 		ASSERT_EQUAL(0, i.func(1));
 		ASSERT_NO_THROW(i.proc(1));
@@ -62,9 +67,12 @@ struct BasicStubbing: tpunit::TestFixture {
 
 		ASSERT_EQUAL(0, i.func(1));
 		ASSERT_NO_THROW(i.proc(1));
+
+		ASSERT_EQUAL(0, i.func(1));
+		ASSERT_NO_THROW(i.proc(1));
 	}
 
-	void stub_a_function_to_return_a_specified_value() {
+	void stub_a_function_to_return_a_specified_value_once() {
 		Mock<SomeInterface> mock;
 
 		When(mock[&SomeInterface::func]).Return(1);
@@ -72,9 +80,23 @@ struct BasicStubbing: tpunit::TestFixture {
 		SomeInterface &i = mock.get();
 
 		ASSERT_EQUAL(1, i.func(1));
+		ASSERT_THROW(i.func(1), fakeit::UnmockedMethodCallException);
+
 	}
 
-	void stub_a_method_to_throw_a_specified_exception() {
+	void stub_a_function_to_return_a_specified_value_always() {
+		Mock<SomeInterface> mock;
+
+		When(mock[&SomeInterface::func]).AlwaysReturn(1);
+
+		SomeInterface &i = mock.get();
+
+		ASSERT_EQUAL(1, i.func(1));
+		ASSERT_EQUAL(1, i.func(1));
+	}
+
+
+	void stub_a_method_to_throw_a_specified_exception_once() {
 		Mock<SomeInterface> mock;
 
 		When(mock[&SomeInterface::func]).Throw(std::string("func exception"));
@@ -93,9 +115,12 @@ struct BasicStubbing: tpunit::TestFixture {
 		} catch (std::string e) {
 			ASSERT_EQUAL(std::string("proc exception"), e);
 		}
+
+		ASSERT_THROW(i.func(1), fakeit::UnmockedMethodCallException);
+		ASSERT_THROW(i.proc(1), fakeit::UnmockedMethodCallException);
 	}
-//
-	void stub_a_method_with_lambda_delegate() {
+
+	void stub_a_method_with_lambda_delegate_once() {
 		Mock<SomeInterface> mock;
 
 		int a = 0;
@@ -107,6 +132,30 @@ struct BasicStubbing: tpunit::TestFixture {
 
 		ASSERT_EQUAL(3, i.func(3));
 
+		i.proc(3);
+		ASSERT_EQUAL(3, a);
+
+		ASSERT_THROW(i.func(3), fakeit::UnmockedMethodCallException);
+		ASSERT_THROW(i.proc(3), fakeit::UnmockedMethodCallException);
+	}
+
+	void stub_a_method_with_lambda_delegate_always() {
+		Mock<SomeInterface> mock;
+
+		int a = 0;
+
+		When(mock[&SomeInterface::func]).AlwaysDo([](int val) {return val;});
+		When(mock[&SomeInterface::proc]).AlwaysDo([&a](int val) {a = val;});
+
+		SomeInterface &i = mock.get();
+
+		ASSERT_EQUAL(3, i.func(3));
+		ASSERT_EQUAL(3, i.func(3));
+
+		i.proc(3);
+		ASSERT_EQUAL(3, a);
+
+		a = 0;
 		i.proc(3);
 		ASSERT_EQUAL(3, a);
 	}
