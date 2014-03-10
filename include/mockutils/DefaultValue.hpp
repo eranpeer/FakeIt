b14c37dd0374f7f030a58e9e1292fe4ef85d4b23
eranpeer <eranpe@gmail.com>
2014-03-10 07:14:43 -0400

    improve exception error message text

--- include/mockutils/DefaultValue.hpp
+++ include/mockutils/DefaultValue.hpp
@@ -6,8 +6,7 @@
 namespace fakeit {
 
 struct DefaultValueInstatiationException: public std::exception {
-	DefaultValueInstatiationException() {
-	}
+	DefaultValueInstatiationException() = default;
 
 	const char* what() const throw () override {
 		return std::string("could not instantiate default value for type").c_str();
@@ -36,16 +35,26 @@ struct DefaultValue {
 	static typename std::enable_if<
 			!std::is_void<C>::value && !std::is_reference<C>::value && !std::is_abstract<C>::value
 					&& !std::is_default_constructible<C>::value, C&>::type value() {
-		throw DefaultValueInstatiationException();
-//		throw std::string(typeid(C).name())
-//				+ std::string(" does not have a default constructor. could not instantiate default value for type");
+
+		class Exception : public DefaultValueInstatiationException {
+			const char* what() const throw () override {
+				return (std::string(typeid(C).name())
+				+ std::string(" does not have a default constructor. could not instantiate default value for type")).c_str();
+			}
+		};
+
+		throw Exception();
 	}
 
 	// Can't create default value of an abstract type
 	template<typename AC>
 	static typename std::enable_if<!std::is_reference<AC>::value && std::is_abstract<AC>::value, AC&>::type value() {
-		throw DefaultValueInstatiationException();
-		//std::string(typeid(AC).name()) + std::string(" is abstract. could not instantiate default value for class");
+		class Exception : public DefaultValueInstatiationException {
+					const char* what() const throw () override {
+						return (std::string(typeid(AC).name()) + std::string(" is abstract. could not instantiate default value for class")).c_str();
+					}
+				};
+		throw Exception();
 	}
 
 	template<typename REF>
