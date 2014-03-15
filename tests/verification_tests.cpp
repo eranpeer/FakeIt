0f1c7391a482999e33e2941da02124ab0e7bb6a3
eranpeer <eranpe@gmail.com>
2014-03-15 14:10:46 -0400

    Split verification_tests.cpp to 2 files

--- tests/verification_tests.cpp
+++ tests/verification_tests.cpp
@@ -44,15 +44,11 @@ struct BasicVerification: tpunit::TestFixture {
 					TEST(BasicVerification::verify_method_was_called_exactly_x_times_with_quantifier), //
 					TEST(BasicVerification::should_throw_IllegalArgumentException_on_negative_times_argument), //
 					TEST(BasicVerification::verify_with_filter), //
-					TEST(BasicVerification::verify_concatenated_sequence), //
-					TEST(BasicVerification::verify_repeated_sequence), //
-					TEST(BasicVerification::verify_repeated_sequence_2), //
-					TEST(BasicVerification::verify_multi_sequences_in_order), TEST(BasicVerification::verify_no_other_invocations_for_mock), //
+					TEST(BasicVerification::verify_no_other_invocations_for_mock), //
 					TEST(BasicVerification::verify_no_other_invocations_for_method_filter), //
 					TEST(BasicVerification::use_same_filter_for_both_stubbing_and_verification), //
 					TEST(BasicVerification::verify_after_paramter_was_changed__with_Matching), //
-					TEST(BasicVerification::verify_after_paramter_was_changed_with_Using), //
-					TEST(BasicVerification::should_throw_argument_exception_on_invalid_repetiotions_number)) //
+					TEST(BasicVerification::verify_after_paramter_was_changed_with_Using)) //
 	{
 	}
 
@@ -307,137 +303,6 @@ struct BasicVerification: tpunit::TestFixture {
 		ASSERT_THROW(Verify(mock[&SomeInterface::func].Using(2)), fakeit::VerificationException);
 	}
 
-	void verify_concatenated_sequence() {
-		Mock<SomeInterface> mock;
-		Fake(mock[&SomeInterface::func], mock[&SomeInterface::proc]);
-		SomeInterface &i = mock.get();
-
-		i.func(1);
-		i.func(2);
-		i.func(3);
-		i.func(4);
-
-		Verify(mock[&SomeInterface::func].Using(1) + mock[&SomeInterface::func].Using(2)).Once();
-		Verify(mock[&SomeInterface::func].Using(2) + mock[&SomeInterface::func].Using(3)).AtLeastOnce();
-		Verify(mock[&SomeInterface::func].Using(3) + mock[&SomeInterface::func].Using(4)).Once();
-		Verify(mock[&SomeInterface::func] + mock[&SomeInterface::func]).Twice();
-		Verify(mock[&SomeInterface::func].Using(1) + mock[&SomeInterface::func].Using(3)).Never();
-
-		ASSERT_THROW(Verify(mock[&SomeInterface::func].Using(1) + mock[&SomeInterface::func].Using(3)), fakeit::VerificationException);
-	}
-
-	void verify_repeated_sequence() {
-		Mock<SomeInterface> mock;
-		Fake(mock[&SomeInterface::func], mock[&SomeInterface::proc]);
-		SomeInterface &i = mock.get();
-
-		i.func(1);
-		i.func(2);
-		i.func(3);
-		i.func(1);
-		i.func(2);
-		i.func(3);
-
-		Verify(mock[&SomeInterface::func] * 1).Exactly(6);
-		Verify(mock[&SomeInterface::func] * 2).Exactly(3);
-		Verify(mock[&SomeInterface::func] * 3).Exactly(2);
-		Verify(mock[&SomeInterface::func] * 4).Exactly(1);
-		Verify(mock[&SomeInterface::func] * 5).Exactly(1);
-		Verify(mock[&SomeInterface::func] * 6).Exactly(1);
-
-		Verify(mock[&SomeInterface::func].Using(1) + mock[&SomeInterface::func].Using(2) + mock[&SomeInterface::func].Using(3)).Twice();
-		Verify((mock[&SomeInterface::func].Using(1) + mock[&SomeInterface::func].Using(2) + mock[&SomeInterface::func].Using(3)) * 2).Once();
-		Verify(mock[&SomeInterface::func].Using(1) * 2).Never();
-
-		ASSERT_THROW(Verify(mock[&SomeInterface::func].Using(1) * 2), fakeit::VerificationException);
-	}
-
-	void verify_repeated_sequence_2() {
-		Mock<SomeInterface> mock;
-		Fake(mock[&SomeInterface::func], mock[&SomeInterface::proc]);
-		SomeInterface &i = mock.get();
-
-		i.func(1);
-		i.func(2);
-		i.func(3);
-		i.func(1);
-		i.func(2);
-		i.func(3);
-
-		Verify(1 * mock[&SomeInterface::func]).Exactly(6);
-		Verify(2 * mock[&SomeInterface::func]).Exactly(3);
-		Verify(3 * mock[&SomeInterface::func]).Exactly(2);
-		Verify(4 * mock[&SomeInterface::func]).Exactly(1);
-		Verify(5 * mock[&SomeInterface::func]).Exactly(1);
-		Verify(6 * mock[&SomeInterface::func]).Exactly(1);
-
-		Verify(2 * mock[&SomeInterface::func].Using(1)).Never();
-		ASSERT_THROW(Verify(2 * mock[&SomeInterface::func].Using(1)), fakeit::VerificationException);
-	}
-
-	void verify_multi_sequences_in_order() {
-		Mock<SomeInterface> mock;
-		Fake(mock[&SomeInterface::func], mock[&SomeInterface::proc]);
-		SomeInterface &i = mock.get();
-
-		i.func(1);
-		i.func(2);
-		i.func(3);
-		i.func(1);
-		i.func(2);
-		i.func(3);
-
-		Verify(  //
-				mock[&SomeInterface::func],  //
-				mock[&SomeInterface::func],  //
-				mock[&SomeInterface::func],  //
-				mock[&SomeInterface::func],  //
-				mock[&SomeInterface::func],  //
-				mock[&SomeInterface::func])  //
-		.Exactly(1);
-
-		Verify(  //
-				mock[&SomeInterface::func],  //
-				mock[&SomeInterface::func],  //
-				mock[&SomeInterface::func],  //
-				mock[&SomeInterface::func],  //
-				mock[&SomeInterface::func])  //
-		.Exactly(1);
-
-		Verify(mock[&SomeInterface::func],  //
-				mock[&SomeInterface::func],  //
-				mock[&SomeInterface::func],  //
-				mock[&SomeInterface::func])  //
-		.Exactly(1);
-
-		Verify(mock[&SomeInterface::func],  //
-				mock[&SomeInterface::func],  //
-				mock[&SomeInterface::func])  //
-		.Exactly(2);
-
-		Verify(mock[&SomeInterface::func],  //
-				mock[&SomeInterface::func])  //
-		.Exactly(3);
-
-		Verify(mock[&SomeInterface::func])  //
-		.Exactly(6);
-
-		Verify(mock[&SomeInterface::func].Using(1) +  //
-				mock[&SomeInterface::func].Using(2) + //
-				mock[&SomeInterface::func].Using(3))  //
-		.Exactly(2);
-
-		Verify(mock[&SomeInterface::func].Using(1) + mock[&SomeInterface::func].Using(2))  //
-		.Exactly(2);
-
-		Verify(mock[&SomeInterface::func].Using(2) + mock[&SomeInterface::func].Using(3))  //
-		.Exactly(2);
-
-		Verify(mock[&SomeInterface::func].Using(1)).Exactly(2);
-
-		Verify(mock[&SomeInterface::func].Using(2) + mock[&SomeInterface::func].Using(1)).Never();
-	}
-
 	void verify_no_other_invocations_for_mock() {
 		Mock<SomeInterface> mock;
 		Fake(mock[&SomeInterface::func], mock[&SomeInterface::proc2]);
@@ -532,15 +397,6 @@ struct BasicVerification: tpunit::TestFixture {
 		a2.state = 2;
 
 		Verify(2 * call_to_proc2_with_state_1);
-	}
-
-	void should_throw_argument_exception_on_invalid_repetiotions_number() {
-		Mock<SomeInterface> mock;
-
-		ASSERT_THROW(Verify(mock[&SomeInterface::func] * 0), std::invalid_argument);
-		ASSERT_THROW(Verify(mock[&SomeInterface::func] * -1), std::invalid_argument);
-		ASSERT_THROW(Verify(0 * mock[&SomeInterface::func]), std::invalid_argument);
-		ASSERT_THROW(Verify(-1 * mock[&SomeInterface::func]), std::invalid_argument);
 	}
 
 } __BasicVerification;
