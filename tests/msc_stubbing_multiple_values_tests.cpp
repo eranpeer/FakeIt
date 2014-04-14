d47cc1918fc07e88b991ab528e29bb788ee289de
eranpeer <eranpe@gmail.com>
2014-04-14 01:24:23 -0400

    Add/Fix Copyright comments

--- tests/msc_stubbing_multiple_values_tests.cpp
+++ tests/msc_stubbing_multiple_values_tests.cpp
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2014 Eran Pe'er.
+ *
+ * This program is made available under the terms of the MIT License.
+ *
+ * Created on Mar 10, 2014
+ */
+
 #include <string>
 #include <queue>
 
