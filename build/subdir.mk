6127a4f161c59833367fc229258f9576e4e6dd64
eranpeer <eranpe@gmail.com>
2014-04-28 16:39:29 -0400

    add a seperate make target for coverage

--- build/subdir.mk
+++ build/subdir.mk
@@ -28,6 +28,20 @@ OBJS += \
 ./verification_tests.o \
 ./verification_error_formatting_tests.o
 
+COVERAGE_OBJS += \
+./cover_default_behaviore_tests.o \
+./cover_gcc_stubbing_multiple_values_tests.o \
+./cover_gcc_type_info_tests.o \
+./cover_msc_stubbing_multiple_values_tests.o \
+./cover_msc_type_info_tests.o \
+./cover_referece_types_tests.o \
+./cover_sequence_verification_tests.o \
+./cover_stubbing_tests.o \
+./cover_tpunit++main.o \
+./cover_type_info_tests.o \
+./cover_verification_tests.o \
+./cover_verification_error_formatting_tests.o
+
 CPP_DEPS += \
 ./default_behaviore_tests.d \
 ./gcc_stubbing_multiple_values_tests.d \
