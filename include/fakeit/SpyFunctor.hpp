ab2c1ab3428597e85f7322ed5885cf66e3e27f35
eranpeer <eranpe@gmail.com>
2014-06-05 08:05:19 -0400

    remove old code

--- include/fakeit/SpyFunctor.hpp
+++ include/fakeit/SpyFunctor.hpp
@@ -25,15 +25,10 @@ public:
 
 	template<typename H>
 	void operator()(const H& head) {
-		H& headWithoutConst = const_cast<H&>(head);
-		headWithoutConst.apply();
 	}
 
 	template<typename H, typename ... M>
 	void operator()(const H& head, const M&... tail) {
-		H& headWithoutConst = const_cast<H&>(head);
-		headWithoutConst.apply();
-		this->operator()(tail...);
 	}
 
 }static Spy;
