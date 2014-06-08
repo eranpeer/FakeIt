cd1a42aab9a11c284db58ee8046b9070629f05fe
eranpeer <eranpe@gmail.com>
2014-06-08 14:12:33 -0400

    refactor, cleanup tests code

--- tests/spying_tests.cpp
+++ tests/spying_tests.cpp
@@ -35,40 +35,40 @@ struct SpyingTests: tpunit::TestFixture {
 
 	void useOriginalClassMethodIfNotFaked() {
 		SomeClass obj;
-		Mock<SomeClass> s(obj);
-		Fake(s[&SomeClass::func]); // Override to return a default value.
-		SomeClass& i = s.get();
+		Mock<SomeClass> spy(obj);
+		Fake(spy[&SomeClass::func]); // Override to return a default value.
+		SomeClass& i = spy.get();
 		ASSERT_EQUAL(0, i.func(1)); // should return default int value (0) 
 		ASSERT_EQUAL(1, i.func2(1)); // should use original method
 	}
 
 	void useOriginalClassMethodIfStubbed() {
 		SomeClass obj;
-		Mock<SomeClass> s(obj);
-		When(s[&SomeClass::func]).AlwaysReturn(10); // Override to return 10
-		SomeClass& i = s.get();
+		Mock<SomeClass> spy(obj);
+		When(spy[&SomeClass::func]).AlwaysReturn(10); // Override to return 10
+		SomeClass& i = spy.get();
 		ASSERT_EQUAL(10, i.func(1)); // should return default int value (0) 
 		ASSERT_EQUAL(1, i.func2(1)); // func2 is not stubbed. should use original method
 	}
 
 	void returnToOrignalClassMethodsOnReset() {
 		SomeClass obj;
-		Mock<SomeClass> s(obj);
-		When(s[&SomeClass::func]).AlwaysReturn(10); // Override to return 10
-		When(s[&SomeClass::func2]).AlwaysReturn(10); // Override to return 10
-		s.Reset();
-		SomeClass& i = s.get();
+		Mock<SomeClass> spy(obj);
+		When(spy[&SomeClass::func]).AlwaysReturn(10); // Override to return 10
+		When(spy[&SomeClass::func2]).AlwaysReturn(10); // Override to return 10
+		spy.Reset();
+		SomeClass& i = spy.get();
 		ASSERT_EQUAL(1, i.func(1));  // should use original method
 		ASSERT_EQUAL(1, i.func2(1)); // should use original method
 	}
 
 	void dataMembersAreNotChangedOnReset() {
 		SomeClass obj;
-		Mock<SomeClass> s(obj);
+		Mock<SomeClass> spy(obj);
 
 		obj.a = 10;
-		s.Reset();
-		SomeClass& i = s.get();
+		spy.Reset();
+		SomeClass& i = spy.get();
 
 		ASSERT_EQUAL(10, obj.a);
 		ASSERT_EQUAL(10, i.a);
