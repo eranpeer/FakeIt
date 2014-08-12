0d3a9418f27d01edb5c189c185d5ba9c080f7498
Joshua Pedrick <joshua.pedrick@ronin-capital.com>
2014-08-12 11:32:52 -0400

    Use fakeit:: namespace inside macros

--- include/fakeit.hpp
+++ include/fakeit.hpp
@@ -17,18 +17,18 @@
 	mock[&std::remove_reference<decltype(mock.get())>::type::method].setMethodDetails(#mock,#method)
 
 #define Verify( ... ) \
-		Verify( __VA_ARGS__ ).setFileInfo(__FILE__,__LINE__, __func__)
+        fakeit::Verify( __VA_ARGS__ ).setFileInfo(__FILE__,__LINE__, __func__)
 
 #define Using( ... ) \
-		Using( __VA_ARGS__ )
+        fakeit::Using( __VA_ARGS__ )
 
 #define VerifyNoOtherInvocations( ... ) \
-	VerifyNoOtherInvocations( __VA_ARGS__ ).setFileInfo(__FILE__,__LINE__, __func__)
+    fakeit::VerifyNoOtherInvocations( __VA_ARGS__ ).setFileInfo(__FILE__,__LINE__, __func__)
 
 #define Fake( ... ) \
-	Fake( __VA_ARGS__ )
+    fakeit::Fake( __VA_ARGS__ )
 
 #define When( call ) \
-	When(call)
+    fakeit::When(call)
 
 #endif // fakeit_h__
