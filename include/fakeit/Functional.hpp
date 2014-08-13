6afbf0916b84effdf5cc859533923374411a5826
Joshua Pedrick <joshua.pedrick@ronin-capital.com>
2014-08-13 10:29:11 -0400

    added const to member function templates

--- include/fakeit/Functional.hpp
+++ include/fakeit/Functional.hpp
@@ -1,6 +1,27 @@
 namespace fakeit{
 
 template< class C, typename R, typename... Args >
+  class const_member_function;
+
+template< class C, typename R, typename... Args >
+  class const_member_function< C, R( Args... ) > {
+    public:
+
+      typedef R ( C::*type )( Args... );
+      typedef const_member_function< C, R( Args... ) > this_type;
+
+      const_member_function( ) {}
+      const_member_function( type m ) : _m(m) {}
+
+      this_type& operator=( type m ){ _m = m; return *this; }
+      R operator() ( C& c, Args&&... args ){ c.*_m( std::forward<Args>(args)... ); }
+      operator bool(){ return _m; }
+      type operator*(){ return _m; }
+    private:
+      type _m = nullptr;
+  };
+
+template< class C, typename R, typename... Args >
   class member_function;
 
 template< class C, typename R, typename... Args >
@@ -33,6 +54,32 @@ template< class C, typename R, typename... Args >
 
       bound_member_function( ) {}
       bound_member_function( C& c, type m ) : _c(&c), _m(m) {}
+
+      this_type& operator=( const std::pair<C&, type>& p ) {
+        _c = &p.first;
+        _m = &p.second;
+        return *this;
+      }
+
+      R operator() ( Args&&... args ){ _c->*_m( std::forward<Args>(args)... ); }
+      operator bool(){ return _m && _c; }
+    private:
+      C* _c;
+      type _m = nullptr;
+  };
+
+template< class C, typename R, typename... Args >
+  class bound_const_member_function;
+
+template< class C, typename R, typename... Args >
+  class bound_const_member_function< C, R( Args... ) > {
+    public:
+
+      typedef R ( C::*type )( Args... );
+      typedef bound_const_member_function< C, R( Args... ) > this_type;
+
+      bound_const_member_function( ) {}
+      bound_const_member_function( C& c, type m ) : _c(&c), _m(m) {}
 
       this_type& operator=( const std::pair<C&, type>& p ) {
         _c = &p.first;
