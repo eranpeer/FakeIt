ab0d9403b085ed9802897f51cd165d8e4916eafe
eranpeer <eranpe@gmail.com>
2014-10-21 23:23:56 +0300

    mend

--- samples/fakeit_with_mstest/unittest1.cpp
+++ samples/fakeit_with_mstest/unittest1.cpp
@@ -2,7 +2,11 @@
 #include "CppUnitTest.h"
 #include "fakeit.hpp"
 
+#include "IGraphic.hpp"
+#include "SquareTool.hpp"
+
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace fakeit;
 
 namespace fakeit_with_mstest
 {		
@@ -12,8 +16,16 @@ namespace fakeit_with_mstest
 		
 		TEST_METHOD(TestMethod1)
 		{
-			
+			Mock<IGraphics> graphicsMock;
+			Fake(Method(graphicsMock, DrawLine));
 
+			SquareTool squareTool;
+			squareTool.setStart(0, 0);
+			squareTool.setSize(10);
+			squareTool.DrawOn(graphicsMock.get());
+
+			Verify(Method(graphicsMock, DrawLine)(0, 0, 10, 0));
+			VerifyNoOtherInvocations(Method(graphicsMock, DrawLine));
 		}
 
 	};
