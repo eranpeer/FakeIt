6ce1f3f98af49712323ec88a904db672ffe5c215
eranpeer <eranpe@gmail.com>
2014-10-21 23:06:38 +0300

    add mstest sample test

---
+++ samples/fakeit_with_mstest/unittest1.cpp
@@ -0,0 +1,20 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include "fakeit.hpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace fakeit_with_mstest
+{		
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			
+
+		}
+
+	};
+}
