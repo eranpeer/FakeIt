fd2d9038b62e617e151317f9b17ee71b6d0a5ab1
eranpeer <eranpe@gmail.com>
2015-07-21 07:50:07 +0300

    add compile tests to prove correct handling of ambiguity in dependent name

--- tests/miscellaneous_tests.cpp
+++ tests/miscellaneous_tests.cpp
@@ -17,112 +17,112 @@
 
 using namespace fakeit;
 
-struct Miscellaneous: tpunit::TestFixture
+struct Miscellaneous : tpunit::TestFixture
 {
-	Miscellaneous() :
-		TestFixture(
-			TEST(Miscellaneous::pass_mock_by_ref), //
-			TEST(Miscellaneous::can_mock_class_without_default_constructor), //
-			TEST(Miscellaneous::can_mock_class_with_protected_constructor), //
-			TEST(Miscellaneous::mock_virtual_methods_of_base_class), //
-			TEST(Miscellaneous::create_and_delete_fakit_instatnce), //
-            TEST(Miscellaneous::testStubFuncWithRightValueParameter), 
-            TEST(Miscellaneous::testStubProcWithRightValueParameter)
-		)
-	{
-	}
+    Miscellaneous() :
+        TestFixture(
+        TEST(Miscellaneous::pass_mock_by_ref), //
+        TEST(Miscellaneous::can_mock_class_without_default_constructor), //
+        TEST(Miscellaneous::can_mock_class_with_protected_constructor), //
+        TEST(Miscellaneous::mock_virtual_methods_of_base_class), //
+        TEST(Miscellaneous::create_and_delete_fakit_instatnce), //
+        TEST(Miscellaneous::testStubFuncWithRightValueParameter),
+        TEST(Miscellaneous::testStubProcWithRightValueParameter)
+        )
+    {
+    }
 
-	struct SomeStruct
-	{
-		SomeStruct(int)
-		{
-		}
+    struct SomeStruct
+    {
+        SomeStruct(int)
+        {
+        }
 
-		virtual void foo() = 0;
-	};
+        virtual void foo() = 0;
+    };
 
-	void can_mock_class_without_default_constructor()
-	{
-		Mock<SomeStruct> mock;
-		Fake(Method(mock,foo));
-	}
+    void can_mock_class_without_default_constructor()
+    {
+        Mock<SomeStruct> mock;
+        Fake(Method(mock, foo));
+    }
 
-	void can_mock_class_with_protected_constructor()
-	{
-		struct SomeClass
-		{
-			virtual void foo() = 0;
-		protected:
-			SomeClass(int)
-			{
-			}
-		};
-		Mock<SomeClass> mock;
-		Fake(Method(mock,foo));
-	}
+    void can_mock_class_with_protected_constructor()
+    {
+        struct SomeClass
+        {
+            virtual void foo() = 0;
+        protected:
+            SomeClass(int)
+            {
+            }
+        };
+        Mock<SomeClass> mock;
+        Fake(Method(mock, foo));
+    }
 
-	void create_and_delete_fakit_instatnce()
-	{
-        class MyFakeit:public DefaultFakeit {
+    void create_and_delete_fakit_instatnce()
+    {
+        class MyFakeit :public DefaultFakeit {
             fakeit::EventHandler &accessTestingFrameworkAdapter() override {
                 throw "not implemented";
             }
         };
-		{
-        	StandaloneFakeit s;
+        {
+            StandaloneFakeit s;
             MyFakeit f;
-		}
-	}
+        }
+    }
 
-	struct Change
-	{
-		virtual void change(uint8_t r, uint8_t g, uint8_t b) =0;
-	};
-
-	void assertChanged(Mock<Change>& mock, uint8_t v1, uint8_t v2, uint8_t v3)
-	{
-		Verify(Method(mock, change).Using(v1,v2,v3));
-	}
-
-	void pass_mock_by_ref()
-	{
-		Mock<Change> mock;
-		Change* change = &mock.get();
-		When(Method(mock, change)).AlwaysReturn();
-		change->change(1, 2, 3);
-		assertChanged(mock, 1, 2, 3);
-	}
-
-	struct A
-	{
-		//~A() = 0;
-		virtual int a1() = 0;
-        virtual int a2() = 0;
-	};
-
-    struct B: public A
+    struct Change
     {
-		virtual ~B() {}
+        virtual void change(uint8_t r, uint8_t g, uint8_t b) = 0;
+    };
+
+    void assertChanged(Mock<Change>& mock, uint8_t v1, uint8_t v2, uint8_t v3)
+    {
+        Verify(Method(mock, change).Using(v1, v2, v3));
+    }
+
+    void pass_mock_by_ref()
+    {
+        Mock<Change> mock;
+        Change* change = &mock.get();
+        When(Method(mock, change)).AlwaysReturn();
+        change->change(1, 2, 3);
+        assertChanged(mock, 1, 2, 3);
+    }
+
+    struct A
+    {
+        //~A() = 0;
+        virtual int a1() = 0;
+        virtual int a2() = 0;
+    };
+
+    struct B : public A
+    {
+        virtual ~B() {}
         virtual int b1() = 0;
         virtual int b2() = 0;
     };
 
     void mock_virtual_methods_of_base_class() {
-		Mock<B> mock;
-        When(Method(mock,b1)).Return(1);
-        When(Method(mock,b2)).Return(2);
-        When(Method(mock,a1)).Return(3);
-        When(Method(mock,a2)).Return(4);
+        Mock<B> mock;
+        When(Method(mock, b1)).Return(1);
+        When(Method(mock, b2)).Return(2);
+        When(Method(mock, a1)).Return(3);
+        When(Method(mock, a2)).Return(4);
 
         ASSERT_EQUAL(1, mock().b1());
         ASSERT_EQUAL(2, mock().b2());
         ASSERT_EQUAL(3, mock().a1());
         ASSERT_EQUAL(4, mock().a2());
-	}
+    }
 
 
-	void testStubFuncWithRightValueParameter() {
-        
+    void testStubFuncWithRightValueParameter() {
+
         struct foo {
             virtual int bar(int &&) = 0;
         };
@@ -166,6 +166,40 @@ struct Miscellaneous: tpunit::TestFixture
         ASSERT_EQUAL(4, rv4);
         foo_mock.get().bar(5);
         ASSERT_EQUAL(5, rv5);
+    }
+
+
+    template <int discriminator>
+    struct DummyType {
+        int value;
+    };
+
+    template <typename T>
+    class ClassToMock {
+    public:
+        virtual unsigned int method() {
+            return 0;
+        }
+    };
+
+    template <typename T>
+    class MockProxyClass {
+        Mock<ClassToMock<T> > mock;
+
+    public:
+        MockProxyClass(unsigned int value) {
+            When(Method(mock, method)).AlwaysReturn(value);
+        }
+
+        Mock<ClassToMock<T> >& getMock() {
+            return mock;
+        }
+    };
+
+    void testRemoveAmbiguityInDependentName()
+    {
+        // this is a compilation test. the following code should compile!!
+        MockProxyClass<DummyType<2> > proxy(3);
     }
 
 } __Miscellaneous;
