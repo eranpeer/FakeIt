0cc933caf3540a98e2287643396fdec336ea2b5e
Alex Merry <alexander.merry@nanoporetech.com>
2016-05-11 17:58:35 +0100

    Add test case for VS2013 crash

---
+++ tests/streaming_tests.cpp
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2016 Oxford Nanopore Technologies
+ *
+ * This program is made available under the terms of the MIT License.
+ *
+ * Created on May 11, 2016
+ */
+
+#include <string>
+
+#include "tpunit++.hpp"
+#include "fakeit.hpp"
+
+using namespace fakeit;
+
+struct StreamingTests : tpunit::TestFixture {
+
+	StreamingTests() :
+	tpunit::TestFixture(
+	TEST(StreamingTests::enum_class_arg)
+	)
+	{
+	}
+    
+    enum class SomeEnum
+    {
+        Item1,
+        Item2
+    };
+
+	struct SomeInterface {
+		virtual void func(SomeEnum) = 0;
+	};
+    
+    void enum_class_arg() {
+        Mock<SomeInterface> mock;
+        When(Method(mock, func)).AlwaysReturn();
+        mock.get().func(SomeEnum::Item1);
+        Verify(Method(mock, func)).Exactly(1);
+    }
+
+} __TypeInfoTests;
