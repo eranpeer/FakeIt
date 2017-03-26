37df12d3bc6c0095102800bf3dc1ad28200b7181
John Galbraith <john.galbraith17@gmail.com>
2017-03-26 03:35:44 -0700

    add integration with the Mettle unit test framework

---
+++ config/mettle/MettleFakeit.hpp
@@ -0,0 +1,66 @@
+#pragma once
+
+#include "fakeit/DefaultFakeit.hpp"
+
+namespace fakeit {
+
+    struct MettleAdapter : public EventHandler {
+
+        std::string formatLineNumner(std::string file, int num){
+#ifndef __GNUG__
+            return file + std::string("(") + std::to_string(num) + std::string(")");
+#else
+            return file + std::string(":") + std::to_string(num);
+#endif
+        }
+
+        virtual ~MettleAdapter() = default;
+
+        MettleAdapter(EventFormatter &formatter)
+            : _formatter(formatter) {
+        }
+
+        virtual void handle(const UnexpectedMethodCallEvent &evt) override {
+            throw mettle::expectation_error(_formatter.format(evt));
+        }
+
+        virtual void handle(const SequenceVerificationEvent &evt) override {
+            throw mettle::expectation_error(
+                    formatLineNumner(evt.file(), evt.line()) + ": " + 
+                    _formatter.format(evt));
+        }
+
+        virtual void handle(const NoMoreInvocationsVerificationEvent &evt) override {
+            throw mettle::expectation_error(
+                    formatLineNumner(evt.file(), evt.line()) + ": " + 
+                    _formatter.format(evt));
+        }
+
+    private:
+        EventFormatter &_formatter;
+    };
+
+    class MettleFakeit : public DefaultFakeit {
+
+    public:
+        virtual ~MettleFakeit() = default;
+
+        MettleFakeit() : _mettleAdapter(*this) {
+        }
+
+        static MettleFakeit &getInstance() {
+            static MettleFakeit instance;
+            return instance;
+        }
+
+    protected:
+
+        fakeit::EventHandler &accessTestingFrameworkAdapter() override {
+            return _mettleAdapter;
+        }
+
+    private:
+
+        MettleAdapter _mettleAdapter;
+    };
+}
