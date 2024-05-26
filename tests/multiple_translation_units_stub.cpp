2b51c7c111dd7ac8f1c93583101489f3a35a37d7
FranckRJ <dev@franckrj.com>
2024-05-26 22:30:58 +0200

    Add back tests from previous implementation.

--- tests/multiple_translation_units_stub.cpp
+++ tests/multiple_translation_units_stub.cpp
@@ -1,12 +1,27 @@
 #include "multiple_translation_units_stub.h"
 
+namespace multiple_tu {
 
-void stubFunc2(fakeit::Mock<SomeInterface>& mock)
-{
-	fakeit::When(Method(mock, func2)).AlwaysReturn<std::string>("String");
-}
+	void stubFunc(fakeit::Mock<SomeInterface>& mock)
+	{
+		fakeit::When(Method(mock, func)).Return(5);
+	}
 
-void stubFunc(fakeit::Mock<SomeInterface>& mock)
-{
-	fakeit::When(Method(mock, func)).AlwaysReturn<int>(3);
+	void stubFunc2(fakeit::Mock<SomeInterface>& mock)
+	{
+		fakeit::When(Method(mock, func2)).Return("String");
+	}
+
+	void stubMoreFunc(fakeit::Mock<SomeInterface>& mock)
+	{
+		fakeit::When(Method(mock, func).Using(1)).Return(10);
+		fakeit::When(Method(mock, func).Using(2)).Return(20);
+	}
+
+	void stubMoreFunc2(fakeit::Mock<SomeInterface>& mock)
+	{
+		fakeit::When(Method(mock, func2).Using(1)).Return("String1");
+		fakeit::When(Method(mock, func2).Using(2)).Return("String2");
+	}
+
 }
